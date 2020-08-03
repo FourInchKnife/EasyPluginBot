cd674f35a54dd3285cddf20da2f3090d7a6279a5
rxal12233445 <Riley.mclain@hotmail.com>
2020-08-03 18:26:29 -0400

    Added setup variables for better control

--- task.py
+++ task.py
@@ -4,7 +4,9 @@ import discord
 from discord.ext import commands
 from time import sleep
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
+#setup variables
+abort_on_no_extensions=True
+bot_command_prefix="!"
 
 print("Checking extensions/ for extensions...")
 for i in os.listdir("extensions"):
@@ -18,9 +20,14 @@ for i in os.listdir("extensions"):
         except NameError:
             print('Improper extension file, missing "cogs" variable. Ignoring and moving on.')
 if bot.cogs == {}:
-    print("No extensions found. Aborting in 5 seconds")
-    sleep(5)
-    exit()
+    print("No extensions found.")
+    if abort_on_no_extensions:
+        print("Aborting in 5 seconds...")
+        sleep(5)
+        exit()
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(bot_command_prefix))
+
 @bot.event
 async def on_connect():
     print("Logged in as {0}".format(bot.user))
