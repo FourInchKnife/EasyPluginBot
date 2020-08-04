aa37c7ef890709adf8a87854b41ac46456856df2
LinuxRocks2000 <LinuxRocks2000>
2020-08-04 15:00:53 -0400

    Improve jokes some more

--- extensions/jokes.py
+++ extensions/jokes.py
@@ -8,15 +8,15 @@ class Joker(commands.Cog):
         self.bot=bot
         self.config=config
         self.jokes=config["ext"]["Jokes"]["jokelist"]
-        self.blacklist=[]
-        self.blacklistlength=config["ext"]["Jokes"]["blacklistlength"]
+        self.usedjokes=[]
     @commands.command()
     async def joke(self,ctx,*args):
-        jokes=[i for i in self.jokes if not i in self.blacklist]
-        joke=random.choice(jokes)
-        self.blacklist.append(joke)
-        if len(self.blacklist)>3:
-            self.blacklist.pop(0)
+        joke=random.choice(self.jokes)
+        self.jokes.remove(joke)
+        self.usedjokes.append(joke)
+        if len(self.jokes)==0:
+            self.jokes=self.usedjokes
+            self.usedjokes=[]
         await ctx.send(joke)
 
 cogs=[Joker]
