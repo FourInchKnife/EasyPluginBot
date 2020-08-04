a96c87c2f833ddc9bb6ff8b74d70e1283e24eb29
rxal12233445 <Riley.mclain@hotmail.com>
2020-08-04 13:49:54 -0400

    Made it work with a config.json

--- run.py
+++ run.py
@@ -3,12 +3,17 @@ import importlib
 import discord
 from discord.ext import commands
 from time import sleep
+import json
+
+with open("config.json","r") as file:
+    config = json.loads(file.read())
 
 #setup variables
-abort_on_no_extensions=True #stops the bot from running if no extensions are loaded
-bot_command_prefix="!" #sets the prefix for all commands tied to the bot
-directory="extensions" #sets the directory for extensions
-owner_discord_id={1} #sets the discord id of the owner(s) of the bot. defaults to clyde (an official discord account)
+abort_on_no_extensions=config["bot"]["no_ext_abort"] #stops the bot from running if no extensions are loaded
+bot_command_prefix=config["bot"]["command_prefix"] #sets the prefix for all commands tied to the bot
+directory=config["bot"]["ext_dir"] #sets the directory for extensions
+owner_discord_id=set(config["bot"]["owner_ids"]) #sets the discord id of the owner(s) of the bot. defaults to clyde (an official discord account)
+bot_token=config["bot"]["bot_token"]
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(bot_command_prefix),owner_ids=owner_discord_id)
 
@@ -25,7 +30,7 @@ for i in os.listdir(directory):
             except NameError:
                 print('Improper extension file, missing "cogs" variable. Ignoring and skipping file.')
         except Exception as e:
-            print("Exception: {} in {}. Ignoring and skipping file.")
+            print("Exception: {} in {}. Ignoring and skipping file.".format(e,file))
 if bot.cogs == {}:
     print("No extensions found.")
     if abort_on_no_extensions:
@@ -42,7 +47,9 @@ async def on_command_error(ctx,err):
     if type(err)!=commands.errors.CommandNotFound:
         await ctx.send(err)
 
-bot_token=os.environ.get('bot_token')
+if not bot_token:
+    bot_token=os.environ.get('bot_token')
 if not bot_token:
     bot_token=input('What is your bot token?')
+
 bot.run(bot_token)
