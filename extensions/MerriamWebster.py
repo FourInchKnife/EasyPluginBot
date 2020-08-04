b3eae3106b19d1e8ab0a3292b02b32969afad3c0
LinuxRocks2000 <LinuxRocks2000>
2020-08-04 16:19:43 -0400

    Started a Merriam Webster utility class

--- extensions/MerriamWebster.py
+++ extensions/MerriamWebster.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import requests as re
+import json
 '''
 This extensions uses the Merriam-Webster dictionary api.
 To actually use this you need to place your api keys in the
@@ -17,10 +19,17 @@ config.json file in the following format:
 You can get your keys by making an account at https://dictionaryapi.com/
 '''
 
+class merriamutils:
+    def getdef(word):
+         return json.loads(re.get("https://www.dictionaryapi.com/api/v3/references/learners/json/dragon?key=4c5a103b-53aa-4459-a276-c90b692f7633")._content)
+
 class MerriamWebster(commands.Cog):
-    '''Not yet implemented :('''
+    '''In dev now...'''
     def __init__(self,bot,config):
         self.bot = bot
         self.config = config
+    @commands.command()
+    async def define(self,ctx,word):
+
 
 cogs=[]
