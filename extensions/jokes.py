f5982efda6dc0c7bf1d1be86b882d25b3c032a2e
LinuxRocks2000 <LinuxRocks2000>
2020-08-04 15:07:26 -0400

    More joke improvement

---
+++ extensions/jokes.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Joker(commands.Cog):
+    def __init__(self,bot,config):
+        self.bot=bot
+        self.config=config
+        self.jokes=config["ext"]["Jokes"]["jokelist"]
+        self.usedjokes=[]
+    @commands.command()
+    async def joke(self,ctx,*args):
+        joke=random.choice(self.jokes)
+        self.jokes.remove(joke)
+        self.usedjokes.append(joke)
+        if len(self.jokes)==0:
+            self.jokes=self.usedjokes
+            self.usedjokes=[]
+        await ctx.send(joke)
+
+cogs=[Joker]
