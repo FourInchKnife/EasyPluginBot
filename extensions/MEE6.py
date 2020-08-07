4abd2c94b33d40a3c77b14e3fc2b951a3e4e5e90
rxal12233445 <Riley.mclain@hotmail.com>
2020-08-07 17:29:02 -0500

    Added the ban command and moved complete extensions to examples

--- extensions/MEE6.py
+++ extensions/MEE6.py
@@ -23,6 +23,17 @@ class Moderation(commands.Cog):
             for i in "bye":
                 await sent_message.add_reaction(makeIndicator(i))
         except discord.HTTPException:
-            await ctx.send("Failed to kick {}. Try making sure that I have the `kick members` role, or move my role to the top of the list.".format(person.mention),allowed_mentions=discord.AllowedMentions(users=False))
+            await ctx.send("Failed to kick {}. Try making sure that I have the `kick members` permission, or move my role to the top of the list.".format(person.mention),allowed_mentions=discord.AllowedMentions(users=False))
+    @commands.command()
+    async def ban(self, ctx, person: discord.Member, *, reason = ""):
+        if not ctx.author.guild_permissions.ban_members:
+            return await ctx.send("No.")
+        try:
+            await person.ban(reason=reason)
+            sent_message = await ctx.send("Banned {}".format(person.mention),allowed_mentions=discord.AllowedMentions(users=False))
+            for i in "ban":
+                await sent_message.add_reaction(makeIndicator(i))
+        except discord.HTTPException:
+            await ctx.send("Failed to ban {}. Try making sure that I have the `ban members` permission, or move my role to the top of the list.".format(person.mention),allowed_mentions=discord.AllowedMentions(users=False))
 
 cogs = [Moderation]
