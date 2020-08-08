8099e65326f55f8c4f87e281aa4ec22a1fdb53dc
rxal12233445 <Riley.mclain@hotmail.com>
2020-08-08 12:20:45 -0500

    updated merriamwebster to work better made it send massive outputs as files when there are errors

--- examples/MerriamWebster.py
+++ examples/MerriamWebster.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import requests as re
 import json
+from io import BytesIO
+
 '''
 This extensions uses the Merriam-Webster dictionary api.
 To actually use this you need to place your api keys in the
@@ -22,7 +24,7 @@ You can get your keys by making an account at https://dictionaryapi.com/
 
 class MerriamWebster(commands.Cog):
     def __init__(self,bot,config,key):
-        self.bot=bot
+        self.bot = bot
         self.key=key
         if not self.key:
             self.key = input("Key for Merriam-Webster Learner's dictionary:")
@@ -32,22 +34,24 @@ class MerriamWebster(commands.Cog):
         data = json.loads(re.get("https://www.dictionaryapi.com/api/v3/references/learners/json/{}?key={}".format(word,self.key))._content)
         embed=discord.Embed(title="Definition of {}".format(word))
         try:
-            for i in data:
-                if word in i["meta"]["app-shortdef"]["hw"]:
-                    header = "{} [{}]".format(i["meta"]["app-shortdef"]["hw"],i["meta"]["app-shortdef"]["fl"])
-                    body=""
-                    for k in i["meta"]["app-shortdef"]["def"]:
-                        q=k
-                        formatting={"{it}":"*","{/it}":"*","{b}":"**","{/b}":"**","{bc}":"_ _\n•","{ldquo}":"“","{rdquo}":"”"}
-                        for i in formatting:
-                            q = q.replace(i, formatting[i])
-                        body+=q+"\n"
-                    embed.add_field(name=header,value=body,inline =False)
-            await ctx.send(embed=embed)
-        except TypeError as e:
-            embed.add_field(name="No definitions found. Maybe try one of these words?",value="•"+"\n•".join(data))
-            await ctx.send(embed=embed)
+            try:
+                for i in data:
+                    if word in i["meta"]["app-shortdef"]["hw"]:
+                        header = "{} [{}]".format(i["meta"]["app-shortdef"]["hw"],i["meta"]["app-shortdef"]["fl"])
+                        body=""
+                        for k in i["meta"]["app-shortdef"]["def"]:
+                            q=k
+                            formatting={"{it}":"*","{/it}":"*","{b}":"**","{/b}":"**","{bc}":"_ _\n•","{ldquo}":"“","{rdquo}":"”"}
+                            for i in formatting:
+                                q = q.replace(i, formatting[i])
+                            body+=q+"\n"
+                        embed.add_field(name=header,value=body,inline =False)
+                await ctx.send(embed=embed)
+            except TypeError as e:
+                embed.add_field(name="No definitions found. Maybe try one of these words?",value="•"+"\n•".join(data))
+                await ctx.send(embed=embed)
         except Exception as e:
-            print("So that threy an error: ```{}```Here's all the data I got: ```{}```".format(e,data))
+            outputFile=discord.File(BytesIO(json.dumps(data, sort_keys=True, indent=4).encode()),filename="output.json")
+            await ctx.send("So that threw an error: ```{}```\nI'll attach all of the data I got.".format(e),file=outputFile)
 
 cogs=[MerriamWebster]
