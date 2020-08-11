7b8e9e214247c7df6ddd825956760f0dd241a857
FourInchKnife <riley.j.mclain@gmail.com>
2020-08-11 13:33:15 -0400

    started work on edit

--- examples/ToDo.py
+++ examples/ToDo.py
@@ -3,24 +3,33 @@ from discord.ext import commands
 import datetime
 import random
 
+if __name__ == '__main__':
+    print("Warn: this file is not meant to be run by the user.")
+
 class ToDo(commands.Cog):
     def __init__(self,bot,config=None,keys=None):
         self.bot=bot
-    async def newID(self, ctx):
-        alph=("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-        IDs=[]
+    async def getLists(self,ctx):
+        IDs={}
         pins = await ctx.channel.pins()
         for i in pins:
             if i.author == ctx.guild.me:
                 try:
-                    IDs.append(i.embeds[0].footer.text)
+                    IDs[(i.embeds[0].footer.text)]=i
                 except:
                     pass
-        IDs.append("000000")
-        testID="000000"
-        while testID in IDs:
+        return IDs
+    async def getList(self,ctx,ID):
+        lists = await self.getLists(ctx)
+        return lists[ID]
+    async def newID(self, ctx):
+        todos=await self.getLists(ctx)
+        while 1:
             testID=random.choice(alph)+random.choice(alph)+random.choice(alph)+random.choice(alph)+random.choice(alph)+random.choice(alph)
-        return testID
+            try:
+                todos[testID]
+            except:
+                return testID
 
     @commands.command()
     async def tdcreate(self,ctx,name,*,desc=''):
@@ -29,5 +38,17 @@ class ToDo(commands.Cog):
         embed.set_footer(text=ID)
         message = await ctx.send(None,embed=embed)
         await message.pin(reason="This message is a ToDo list")
+    @commands.command()
+    async def tdedit(self,ctx,listID,operation,*,value):
+        operations=[]
+        if not operation in operations:
+            await ctx.send("`{}` is not a valid operation".format(operation))
+            return
+        if ':' in listID:
+            list = (await getList(listID.split(':')[0]), listID.split(':')[1])
+
+        else:
+            list = (await getList(listID),None)
+
 
 cogs=[ToDo]
